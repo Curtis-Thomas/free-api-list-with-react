d44c4c6531dd4ecad960e9a9e1430daac5f6f143
Curtis Thomas <83663235+fuzzyduckers@users.noreply.github.com>
2023-06-10 10:50:21 +0200

    style: add BtnRouting responsive styling

--- src/core/main/BtnRouting.js
+++ src/core/main/BtnRouting.js
@@ -1,16 +1,25 @@
 import { useNavigate } from "react-router-dom";
-import { Box, Button } from "@mui/material";
+import { Box, Button, useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 const BtnRouting = ({ btnName, navigateTo }) => {
   const navigate = useNavigate();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   const handleClick = () => {
     navigate(navigateTo);
   };
 
   return (
-    <Box sx={{ border: "solid 1px black", width: "33vw" }}>
-      <Button fullWidth="true" onClick={handleClick}>
+    <Box
+      sx={{
+        border: "solid 1px black",
+        width: isMobile ? "100%" : "33vw",
+        maxWidth: "100%",
+      }}
+    >
+      <Button fullWidth={true} onClick={handleClick}>
         {btnName}
       </Button>
     </Box>
