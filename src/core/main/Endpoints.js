03b6bcce0c01d8961dd3f73b99cf7b97f012f4d2
Curtis Thomas <83663235+fuzzyduckers@users.noreply.github.com>
2023-06-10 11:11:52 +0200

    feat: add btn copy endpoint, add btn copy example

--- src/core/main/Endpoints.js
+++ src/core/main/Endpoints.js
@@ -1,22 +1,52 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 
 const Endpoints = ({ header, endpoint, description, example }) => {
+  const handleCopyEndpoint = () => {
+    navigator.clipboard
+      .writeText(endpoint)
+      .then(() => {
+        console.log("Copied endpoint to clipboard:", endpoint);
+      })
+      .catch((error) => {
+        console.error("Failed to copy endpoint to clipboard:", error);
+      });
+  };
+
+  const handleCopyExample = () => {
+    navigator.clipboard
+      .writeText(example)
+      .then(() => {
+        console.log("Copied example to clipboard:", example);
+      })
+      .catch((error) => {
+        console.error("Failed to copy example to clipboard:", error);
+      });
+  };
+
   return (
     <Box>
       <Typography>Endpoint</Typography>
-      <br></br>
+      <br />
 
-      <Typography>
-        {header} {endpoint}
-      </Typography>
+      <Typography>{header}</Typography>
+      <br />
+      <Typography>{endpoint}</Typography>
+      <Button variant="outlined" onClick={handleCopyEndpoint}>
+        Copy Endpoint
+      </Button>
+      <br />
+      <br />
 
       <Typography>{description}</Typography>
-      <br></br>
+      <br />
 
       <Typography>Example Request</Typography>
-      <br></br>
+      <br />
 
       <Typography>{example}</Typography>
+      <Button variant="outlined" onClick={handleCopyExample}>
+        Copy Example
+      </Button>
     </Box>
   );
 };
