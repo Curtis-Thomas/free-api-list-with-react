4b7e48f69cc6b167a6980cdb46996de1c0ef73f2
Curtis Thomas <83663235+fuzzyduckers@users.noreply.github.com>
2023-06-10 13:23:47 +0200

    feat: add home page info

--- src/core/main/Main.js
+++ src/core/main/Main.js
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 import BtnRouting from "./BtnRouting";
 import NavBar from "./navigation/NavBar";
@@ -7,6 +7,14 @@ const Main = () => {
   return (
     <Box>
       <NavBar />
+      <Box sx={{ textAlign: "center", marginBottom: 5, marginTop: 5 }}>
+        <Typography>
+          Welcome to the Free API List with React <br />
+          <br /> Discover a comprehensive collection of free APIs across
+          different categories and explore their endpoints, request parameters,
+          and response formats.
+        </Typography>
+      </Box>
 
       <Box>
         <BtnRouting btnName="Animals" navigateTo="/AnimalsDash" />
