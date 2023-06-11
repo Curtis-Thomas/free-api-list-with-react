6332c31c9aa2d91dc2e6bce750668d64eb579456
pchawlaji <58567694+pchawlaji@users.noreply.github.com>
2023-06-11 16:12:02 +0530

    Issue#13
    Added Documentation.js file with basic skeleton.

---
+++ src/documentation/Documentation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+/**
+ * The Documentation component returns a simple div element containing an h1 heading. 
+ *  Add your documentation content or components within the div as needed.
+*/
+const Documentation = () => {
+    return (
+        <div>s
+            <h1>Documentation Page</h1>
+            {/* Add your content here */}
+        </div>
+    );
+};
+
+export default Documentation;
+
