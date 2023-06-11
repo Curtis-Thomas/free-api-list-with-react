b1981534898ed6c78dc72d323e8118211cb4ca57
pchawlaji <58567694+pchawlaji@users.noreply.github.com>
2023-06-11 17:25:19 +0530

    Update Footer Styling #3
    Added Footer file

--- src/core/footer/Footer.js
+++ src/core/footer/Footer.js
@@ -1,9 +1,23 @@
-import { Box, Typography } from "@mui/material";
+import React from 'react';
+import { Box, Typography } from '@mui/material';
 
 const Footer = () => {
   return (
-    <Box>
-      <Typography>Footer</Typography>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        minHeight: '5vh',
+        justifyContent: 'center',
+        backgroundColor: '#313335',
+        color: 'white',
+        textAlign: 'center',
+        padding: 2,
+      }}
+    >
+      <Typography>
+       Copyright 2023 - 2023, Curtis Thomas and the Free API List with React contributors.
+      </Typography>
     </Box>
   );
 };
