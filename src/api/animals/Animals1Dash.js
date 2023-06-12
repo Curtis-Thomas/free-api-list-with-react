0ca2710d4ee4be89f674d66b9eb36c32b48f7416
Curtis Thomas <83663235+fuzzyduckers@users.noreply.github.com>
2023-06-12 23:06:34 +0200

    docs: api/animals/Animals1Dash docs added with comments

--- src/api/animals/Animals1Dash.js
+++ src/api/animals/Animals1Dash.js
@@ -5,32 +5,43 @@ import NavBar from "../../core/main/navigation/NavBar";
 const AnimalDash = () => {
   return (
     <Box>
+      {/* Navigation bar */}
       <NavBar link="/AnimalsDash" />
+
       <Box>
+        {/* Button for Daily Cat Facts */}
         <BtnRouting
           btnName="Daily Cat Facts"
           navigateTo="AnimalsDailyCatFacts"
           backgroundColor="#0077ba"
           textColor="white"
         />
+
+        {/* Button for Cat as a service */}
         <BtnRouting
           btnName="Cat as a service"
           navigateTo="AnimalsCataas"
           backgroundColor="#0077ba"
           textColor="white"
         />
+
+        {/* Button for Dog API */}
         <BtnRouting
-          btnName="Dog Api"
+          btnName="Dog API"
           navigateTo="AnimalsDogApi"
           backgroundColor="#0077ba"
           textColor="white"
         />
+
+        {/* Button for Dog */}
         <BtnRouting
           btnName="Dog"
           navigateTo="AnimalsDog"
           backgroundColor="#0077ba"
           textColor="white"
         />
+
+        {/* Button for Meow Facts */}
         <BtnRouting
           btnName="Meow Facts"
           navigateTo="AnimalsMeowFacts"
