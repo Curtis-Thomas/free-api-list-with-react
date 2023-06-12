86911d6baf861edfaa3f9bec5d613ce32c81006c
Curtis Thomas <83663235+fuzzyduckers@users.noreply.github.com>
2023-06-12 08:32:47 +0200

    add: tools page

---
+++ src/tools/Tools.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+const Tools = () => {
+  return (
+    <Box>
+      <Typography variant="h2">Tools Page</Typography>
+    </Box>
+  );
+};
+
+export default Tools;
