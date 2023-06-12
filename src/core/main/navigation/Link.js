842217951cc18db9a2c97b1a382630b8d152b750
jibranabsarulislam <jga26@njit.edu>
2023-06-12 18:06:11 -0400

    use mui appbar+toolbar for navbar. enhance link and visuals

--- src/core/main/navigation/Link.js
+++ src/core/main/navigation/Link.js
@@ -1,15 +1,56 @@
 // https://stackoverflow.com/questions/63216730/can-you-use-material-ui-link-with-react-router-dom-link
 
-import { Link as MuiLink } from "@mui/material";
-import { Link as ReactRouterLink } from "react-router-dom";
-
 import React from "react";
 
+import { Link as ReactRouterLink } from "react-router-dom";
+
+import { Button } from "@mui/material";
+
+const onMouseOver = (e) => {
+  e.target.style.backgroundColor = "#a1a1a1";
+};
+
+const onMouseOut = (e) => {
+  e.target.style.removeProperty("background-color");
+};
+
+const linkStyle = {
+  textOverflow: "ellipsis",
+  whiteSpace: "nowrap",
+  padding: "0 0.5rem",
+  overflow: "hidden",
+  textDecoration: "none",
+  color: "black",
+};
+
 const Link = (props) => {
+  let label = props.label;
+
+  if (label[0] === "/") {
+    label = label.slice(1);
+  }
+
   return (
-    <MuiLink {...props} component={ReactRouterLink} to={props.href ?? "#"}>
-      {props.children}
-    </MuiLink>
+    <Button
+      {...props}
+      component={ReactRouterLink}
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
+      aria-label={`nav-${label}`}
+      onKeyPress={(e) => {
+        /* add spacebar for accessibility
+        this is on by default for buttons,
+        but since ReactRouterLink is being used,
+        it needs to be added manually */
+        if (e.key === " ") {
+          e.preventDefault();
+          e.target.click();
+        }
+      }}
+      style={linkStyle}
+    >
+      {label}
+    </Button>
   );
 };
 
--- src/core/main/navigation/NavBar.js
+++ src/core/main/navigation/NavBar.js
@@ -1,78 +1,41 @@
 import React from "react";
-import Box from "@mui/material/Box";
+
+import { AppBar, Toolbar, Box } from "@mui/material";
 
 import Link from "./Link";
 
 const NavBar = ({ link, route2 }) => {
-  const handleMouseOver = (event) => {
-    event.target.style.color = "#ffffff";
-  };
-
-  const handleMouseOut = (event) => {
-    event.target.style.color = "#000000";
-  };
-
-  const linkStyle = {
-    padding: 2,
-    display: "block",
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    whiteSpace: "nowrap",
-    color: "#313335",
-    textDecoration: "none",
-    textAlign: "center",
-  };
-
   return (
-    <Box
-      sx={{
-        display: "flex",
-        marginBottom: 3,
-        height: "2.5rem",
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "space-evenly",
-        backgroundColor: "#a1a1a1",
-      }}
-    >
-      <Box
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-        sx={{ flex: 3 }}
-      >
-        <Link href="/" style={linkStyle}>
-          Home
-        </Link>
-      </Box>
-      {link && (
-        <>
-          <Box sx={{ flex: 1, textAlign: "right" }}>Category:</Box>
-          <Box
-            onMouseOver={handleMouseOver}
-            onMouseOut={handleMouseOut}
-            sx={{ flex: 1 }}
-          >
-            <Link href={link} style={linkStyle}>
-              <b>{link}</b>
-            </Link>
+    <>
+      <AppBar position="static" style={{ backgroundColor: "#bbb" }}>
+        <Toolbar>
+          <Box sx={{ flexGrow: 1 }}>
+            <Link label="Home" to={"/"} />
           </Box>
-        </>
-      )}
-      {route2 && (
-        <>
-          <Box sx={{ flex: 1, textAlign: "right" }}>API:</Box>
-          <Box
-            onMouseOver={handleMouseOver}
-            onMouseOut={handleMouseOut}
-            sx={{ flex: 1 }}
-          >
-            <Link style={linkStyle}>
-              <b>{route2}</b>
-            </Link>
+          <Box>
+            {/* since each 'category' link has dash at the end, 
+              remove it before passing to Link component
+              for clarity */}
+            {link && <Link label={link.slice(0, -4)} to={link} />}
+            {route2 && (
+              <>
+                <span
+                  style={{
+                    color: "black",
+                    pointerEvents: "none",
+                    tabIndex: "-1",
+                  }}
+                >
+                  {" ▶ "}
+                </span>
+                <Link label={route2} to="#" />
+              </>
+            )}
           </Box>
-        </>
-      )}
-    </Box>
+        </Toolbar>
+      </AppBar>
+      <br />
+    </>
   );
 };
 
