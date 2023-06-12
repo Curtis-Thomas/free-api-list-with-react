2e1a5b36252007ed3970b9fa8b21da9ce18f62b7
Curtis Thomas <83663235+fuzzyduckers@users.noreply.github.com>
2023-06-12 08:29:34 +0200

    feat: add to header, btn tools, btn docs

--- src/core/header/Header.js
+++ src/core/header/Header.js
@@ -1,23 +1,22 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 
 const Header = () => {
   return (
     <Box
       sx={{
-        display: "flex",
         alignItems: "center",
         minHeight: "5vh",
-        justifyContent: "center",
         backgroundColor: "#313335",
         color: "white",
         textAlign: "center",
         padding: 2,
       }}
     >
-      <Typography>
-        Free API List with React, a collection of free APIs across different
-        categories. Btn White = Empty
-      </Typography>
+      <Typography>Free API List with React</Typography>
+      <Box>
+        <Button>Tools</Button>
+        <Button>Docs</Button>
+      </Box>
     </Box>
   );
 };
