8b2651462d1b7657d5a969477577819bd8ff7137
Curtis Thomas <83663235+fuzzyduckers@users.noreply.github.com>
2023-06-13 10:51:42 +0200

    style: ArtDesign1Dash btn style set to default

--- src/api/artDesign/ArtDesign1Dash.js
+++ src/api/artDesign/ArtDesign1Dash.js
@@ -7,18 +7,8 @@ const ArtDesignDash = () => {
     <Box>
       <NavBar link="/ArtDesignDash" />
       <Box>
-        <BtnRouting
-          btnName="Artic"
-          navigateTo="ArtDesignArtic"
-          backgroundColor="#0077ba"
-          textColor="white"
-        />
-        <BtnRouting
-          btnName="EmojiHub"
-          navigateTo="ArtDesignEmojiHub"
-          backgroundColor="#0077ba"
-          textColor="white"
-        />
+        <BtnRouting btnName="Artic" navigateTo="ArtDesignArtic" />
+        <BtnRouting btnName="EmojiHub" navigateTo="ArtDesignEmojiHub" />
       </Box>
     </Box>
   );
