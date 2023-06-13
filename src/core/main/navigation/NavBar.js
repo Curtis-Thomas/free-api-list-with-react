0ff717da984d561ebb33e03d9e1366dec7a02f63
Curtis Thomas <83663235+fuzzyduckers@users.noreply.github.com>
2023-06-13 16:59:42 +0200

    style: Navbar Span color set to default to be more visible on dark mode

--- src/core/main/navigation/NavBar.js
+++ src/core/main/navigation/NavBar.js
@@ -27,7 +27,6 @@ const NavBar = ({ link, route2 }) => {
               <>
                 <span
                   style={{
-                    color: "black",
                     pointerEvents: "none",
                     tabIndex: "-1",
                   }}
