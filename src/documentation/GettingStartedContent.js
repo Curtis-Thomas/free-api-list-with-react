fbfa3397d83975c444988c47c0dbf4413cd0dd17
MamathaKarapanahalli <contact.mamathak@gmail.com>
2023-07-12 12:05:43 +0530

    Update GettingStartedContent.js

--- src/documentation/GettingStartedContent.js
+++ src/documentation/GettingStartedContent.js
@@ -4,7 +4,7 @@ import { Box, Typography } from "@mui/material";
 function GettingStartedContent() {
   return (
     <Box sx={{ border: "solid 1px #333333" }}>
-      <Typography>Getting Started Content_Test12thJuly</Typography>
+      <Typography>Getting Started Content_Test12thJuly1</Typography>
     </Box>
   );
 }
