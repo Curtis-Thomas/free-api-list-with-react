6aca7f24ee845c307748dd17af4188368e5eda72
MamathaKarapanahalli <contact.mamathak@gmail.com>
2023-07-12 11:53:30 +0530

    Update GettingStartedContent.js
    Testing commit from desktop app

--- src/documentation/GettingStartedContent.js
+++ src/documentation/GettingStartedContent.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
 
-const GettingStartedContent = () => {
+function GettingStartedContent() {
   return (
     <Box sx={{ border: "solid 1px #333333" }}>
-      <Typography>Getting Started Content</Typography>
+      <Typography>Getting Started Content_Test12thJuly</Typography>
     </Box>
   );
-};
+}
 
 export default GettingStartedContent;
