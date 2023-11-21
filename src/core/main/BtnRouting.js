5793a13fa99bc3aaccca05835470bb6730e38f86
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2023-11-21 14:37:24 +0200

    style: btnrouting style update

--- src/core/main/BtnRouting.js
+++ src/core/main/BtnRouting.js
@@ -1,14 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { Box, Button, Typography } from "@mui/material";
 
-/**
- * Component for a routing button that navigates to a specified route.
- *
- * @param {string} btnName - The name or label for the button.
- * @param {string} navigateTo - The path or route to navigate to when the button is clicked.
- * @param {string} backgroundColor - The background color of the button.
- * @param {string} textColor - The text color of the button.
- */
 const BtnRouting = ({
   btnName,
   navigateTo,
@@ -18,9 +10,6 @@ const BtnRouting = ({
 }) => {
   const navigate = useNavigate();
 
-  /**
-   * Handles the click event of the button and navigates to the specified route.
-   */
   const handleClick = () => {
     navigate(navigateTo);
   };
@@ -32,30 +21,28 @@ const BtnRouting = ({
         width: "95%",
         maxWidth: "100%",
         marginBottom: 2,
-        borderRadius: "3px",
         marginLeft: "auto",
         marginRight: "auto",
       }}
     >
       <Button
-        fullWidth={true}
+        fullWidth
         onClick={handleClick}
         sx={{
           color: textColor,
           backgroundColor: backgroundColor,
           "&:hover": {
             backgroundColor: backgroundColor,
+            opacity: 0.9,
           },
         }}
       >
-        {btnName}
         <Typography
-          sx={{
-            fontWeight: "bold",
-            paddingLeft: 0.5,
-          }}
+          variant="button"
+          sx={{ fontWeight: "bold", textTransform: "none" }}
         >
-          {amount}
+          {btnName}{" "}
+          {amount && <span style={{ paddingLeft: 0.5 }}>{amount}</span>}
         </Typography>
       </Button>
     </Box>
