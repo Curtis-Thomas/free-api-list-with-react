96601c32fc5ee90d7b2ca4df289ceba21cfaa7e3
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2023-11-21 14:51:56 +0200

    refactor: link code clean up

--- src/core/main/navigation/Link.js
+++ src/core/main/navigation/Link.js
@@ -1,54 +1,34 @@
-// https://stackoverflow.com/questions/63216730/can-you-use-material-ui-link-with-react-router-dom-link
-
 import React from "react";
-
 import { Link as ReactRouterLink } from "react-router-dom";
-
 import { Button } from "@mui/material";
 
-const onMouseOver = (e) => {
-  e.target.style.backgroundColor = "#a1a1a1";
-};
-
-const onMouseOut = (e) => {
-  e.target.style.backgroundColor = "#bbb";
-};
-
-const linkStyle = {
-  padding: "0 0.5rem",
-  textDecoration: "none",
-  color: "black",
-  border: "none",
-  backgroundColor: "#bbb",
-};
-
-const Link = (props) => {
-  let label = props.label;
-
-  if (label[0] === "/") {
-    label = label.slice(1);
-  }
+const Link = ({ label, ...props }) => {
+  const formattedLabel = label[0] === "/" ? label.slice(1) : label;
 
   return (
     <Button
       {...props}
       component={ReactRouterLink}
-      onMouseOver={onMouseOver}
-      onMouseOut={onMouseOut}
-      aria-label={`nav-${label}`}
+      variant="text"
+      sx={{
+        padding: "0 0.5rem",
+        color: "black",
+        backgroundColor: "#bbb",
+        "&:hover": {
+          backgroundColor: "#a1a1a1",
+        },
+        textDecoration: "none",
+        border: "none",
+      }}
+      aria-label={`nav-${formattedLabel}`}
       onKeyPress={(e) => {
-        /* add spacebar for accessibility
-        this is on by default for buttons,
-        but since ReactRouterLink is being used,
-        it needs to be added manually */
-        if (e.key === " ") {
+        if (e.key === " " || e.key === "Enter") {
           e.preventDefault();
           e.target.click();
         }
       }}
-      style={linkStyle}
     >
-      {label}
+      {formattedLabel}
     </Button>
   );
 };
