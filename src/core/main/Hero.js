eef8d2835ae49f0ae987776ead0324e514c1505f
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2023-11-21 14:46:20 +0200

    style: hero style update

--- src/core/main/Hero.js
+++ src/core/main/Hero.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Typography, Grid } from "@mui/material";
 import GitHubButton from "react-github-btn";
 import DescriptionIcon from "@mui/icons-material/Description";
 
@@ -11,6 +10,7 @@ function Hero() {
   const handleDocsClick = () => {
     navigate("Docs");
   };
+
   return (
     <Box
       sx={{
@@ -20,66 +20,47 @@ function Hero() {
         justifyContent: "center",
         alignItems: "center",
         height: "10vh",
+        padding: 2,
       }}
     >
-      <Box
-        sx={{
-          width: "50%",
-          backgroundColor: "#24292e",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "10vh",
-        }}
-      >
-        <Typography variant="h6" sx={{ marginRight: "16px", color: "white" }}>
-          GitHub
-        </Typography>
-        <GitHubButton
-          href="https://github.com/Curtis-Thomas/free-api-list-with-react"
-          data-icon="octicon-star"
-          aria-label="Star buttons/github-buttons on GitHub"
-          data-show-count="true"
-          data-size="large"
-          data-count-href="/Curtis-Thomas/free-api-list-with-react/stargazers"
-          data-count-api="/repos/Curtis-Thomas/free-api-list-with-react#stargazers_count"
-          data-count-aria-label="# stargazers on GitHub"
-        >
-          Star
-        </GitHubButton>
-      </Box>
-      <Box
-        sx={{
-          width: "50%",
-          backgroundColor: "#24292e",
-
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "10vh",
-        }}
-      >
-        <Button
-          onClick={handleDocsClick}
-          sx={{
-            width: "50%",
-            backgroundColor: "#24292e",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            border: "solid 1px #BBBBBB",
-            color: "#BBBBBB",
-          }}
-        >
-          <Typography
-            variant="h6"
-            sx={{ marginRight: "16px", color: "#BBBBBB" }}
-          >
-            Docs
+      <Grid container alignItems="center" justifyContent="center">
+        <Grid item xs={6} display="flex" justifyContent="center">
+          <Typography variant="h6" sx={{ marginRight: 2, color: "white" }}>
+            GitHub
           </Typography>
-          <DescriptionIcon />
-        </Button>
-      </Box>
+          <GitHubButton
+            href="https://github.com/Curtis-Thomas/free-api-list-with-react"
+            data-icon="octicon-star"
+            aria-label="Star buttons/github-buttons on GitHub"
+            data-show-count="true"
+            data-size="large"
+            data-count-href="/Curtis-Thomas/free-api-list-with-react/stargazers"
+            data-count-api="/repos/Curtis-Thomas/free-api-list-with-react#stargazers_count"
+            data-count-aria-label="# stargazers on GitHub"
+          >
+            Star
+          </GitHubButton>
+        </Grid>
+        <Grid item xs={6} display="flex" justifyContent="center">
+          <Button
+            onClick={handleDocsClick}
+            startIcon={<DescriptionIcon />}
+            sx={{
+              border: "1px solid #BBBBBB",
+              color: "#BBBBBB",
+              backgroundColor: "#343a40",
+              "&:hover": {
+                color: "white",
+                borderColor: "white",
+              },
+            }}
+          >
+            <Typography variant="h6" sx={{ color: "#BBBBBB" }}>
+              Docs
+            </Typography>
+          </Button>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
