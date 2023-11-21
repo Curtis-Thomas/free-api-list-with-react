9663b11d4d707f00f5fc534f817bfd897b328881
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2023-11-21 14:31:47 +0200

    style: base url box update

--- src/core/main/BoxApiInfo.js
+++ src/core/main/BoxApiInfo.js
@@ -1,47 +1,72 @@
-import { Typography, Box, Button } from "@mui/material";
+import React, { useState } from "react";
+import { Typography, Box, Button, Snackbar, IconButton } from "@mui/material";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import CloseIcon from "@mui/icons-material/Close";
 
-/**
- * Component for displaying API information, including a base URL and a copy button.
- *
- * @param {string} baseEndpoint - The base URL of the API.
- */
 const BoxApiInfo = ({ baseEndpoint }) => {
-  /**
-   * Handles the click event of the copy button and copies the base URL to the clipboard.
-   */
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+
   const handleCopyClick = () => {
     navigator.clipboard
       .writeText(baseEndpoint)
       .then(() => {
         console.log("Copied to clipboard:", baseEndpoint);
+        setOpenSnackbar(true);
       })
       .catch((error) => {
         console.error("Failed to copy to clipboard:", error);
       });
   };
 
+  const handleCloseSnackbar = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenSnackbar(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleCloseSnackbar}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
   return (
     <Box
       sx={{
-        height: "5vh",
-        marginTop: "5vh",
-        marginBottom: "10vh",
+        padding: 2,
+        marginBottom: 3,
         display: "flex",
-        justifyContent: "space-evenly",
+        justifyContent: "space-between",
         alignItems: "center",
+        border: "1px solid #bbb",
       }}
     >
-      <Box>
-        <Typography>Base URL:</Typography>
-      </Box>
-      <Box>
-        <Typography>{baseEndpoint}</Typography>
-      </Box>
-      <Box>
-        <Button variant="outlined" onClick={handleCopyClick}>
-          Copy
-        </Button>
-      </Box>
+      <Typography variant="subtitle1">Base URL:</Typography>
+      <Typography variant="body2" sx={{ marginRight: 2 }}>
+        {baseEndpoint}
+      </Typography>
+      <Button
+        variant="outlined"
+        startIcon={<ContentCopyIcon />}
+        onClick={handleCopyClick}
+      >
+        Copy
+      </Button>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        message="URL copied to clipboard"
+        action={action}
+      />
     </Box>
   );
 };
