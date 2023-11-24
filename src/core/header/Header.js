faf9f971b18ad1c4146f09a93c04540a8968e86f
STEFANOS PANAGIOTIS GIANNAKOS <gstefenp@outlook.com>
2023-11-24 23:14:31 +0200

    Created a menu for the tools. docs, night mode switch. Made the switch stay toggled when you close the menu, depending on the current theme. Added descriptions next to the icons and margins.

--- src/core/header/Header.js
+++ src/core/header/Header.js
@@ -1,3 +1,4 @@
+import React, { } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import {
   AppBar,
@@ -6,31 +7,52 @@ import {
   Typography,
   IconButton,
   Stack,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 
 import BuildIcon from "@mui/icons-material/Build";
 import DescriptionIcon from "@mui/icons-material/Description";
+import MenuIcon from '@mui/icons-material/Menu';
 
 /**
-
 Component for the header of the application.
 */
 const Header = ({ toggleDarkMode }) => {
   const navigate = useNavigate();
-  /**
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [darkMode, setDarkMode] = React.useState(false);
 
+
+  /**
 Handles the click event of the "Tools" button and navigates to the "Tools" route.
 */
   const handleToolsClick = () => {
     navigate("Tools");
+    handleMenuClose();
   };
   /**
-
 Handles the click event of the "Docs" button and navigates to the "Docs" route.
 */
   const handleDocsClick = () => {
     navigate("Docs");
+    handleMenuClose();
   };
+
+  /**Handles the click event of the Menu button. Opens and closes the Menu*/
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleSwitchToggle = () => {
+    setDarkMode(!darkMode); //Toggle the state of the switch
+    toggleDarkMode();
+  };
+
   return (
     <AppBar position="fixed">
       <Toolbar>
@@ -50,14 +72,27 @@ Handles the click event of the "Docs" button and navigates to the "Docs" route.
             </Typography>
           </div>
           <div style={{ textAlign: "right" }}>
-            <Stack direction="row" spacing={1}>
-              <IconButton onClick={handleToolsClick} edge="end">
-                <BuildIcon />
+            <Stack direction="row" alignItems="center">
+              <IconButton onClick={handleMenuClick} edge="start">
+                <MenuIcon />
               </IconButton>
-              <IconButton onClick={handleDocsClick} edge="end">
-                <DescriptionIcon />
-              </IconButton>
-              <Switch onChange={toggleDarkMode} />
+              <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleMenuClose}
+              >
+                <MenuItem onClick={handleToolsClick}>
+                  <BuildIcon sx={{ marginRight: 1 }} />
+                  <Typography variant="body1">Tools</Typography>
+                </MenuItem>
+                <MenuItem onClick={handleDocsClick}>
+                  <DescriptionIcon sx={{ marginRight: 1 }} />
+                  <Typography variant="body1">Docs</Typography>
+                </MenuItem>
+                <MenuItem>
+                  <Switch checked={darkMode} onChange={handleSwitchToggle} />
+                </MenuItem>
+              </Menu>
             </Stack>
           </div>
         </div>
