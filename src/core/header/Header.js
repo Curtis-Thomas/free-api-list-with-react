3eab157b2c21d7af013fafe9065b48737263885b
STEFANOS PANAGIOTIS GIANNAKOS <gstefenp@outlook.com>
2023-11-25 10:49:22 +0200

    Reverted the stack of icons back to the original stack, added margin as well for a cleaner look. Made the menu icon apper only on mobile devises, max 712px.

--- src/core/header/Header.js
+++ src/core/header/Header.js
@@ -14,6 +14,10 @@ import {
 import BuildIcon from "@mui/icons-material/Build";
 import DescriptionIcon from "@mui/icons-material/Description";
 import MenuIcon from '@mui/icons-material/Menu';
+import Hidden from "@mui/material/Hidden";
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+
 
 /**
 Component for the header of the application.
@@ -23,10 +27,11 @@ const Header = ({ toggleDarkMode }) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [darkMode, setDarkMode] = React.useState(false);
 
-
+  const isMobile = useMediaQuery('(max-width: 712px)');
   /**
 Handles the click event of the "Tools" button and navigates to the "Tools" route.
 */
+
   const handleToolsClick = () => {
     navigate("Tools");
     handleMenuClose();
@@ -72,28 +77,42 @@ Handles the click event of the "Docs" button and navigates to the "Docs" route.
             </Typography>
           </div>
           <div style={{ textAlign: "right" }}>
-            <Stack direction="row" alignItems="center">
-              <IconButton onClick={handleMenuClick} edge="start">
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={handleMenuClose}
-              >
-                <MenuItem onClick={handleToolsClick}>
-                  <BuildIcon sx={{ marginRight: 1 }} />
-                  <Typography variant="body1">Tools</Typography>
-                </MenuItem>
-                <MenuItem onClick={handleDocsClick}>
-                  <DescriptionIcon sx={{ marginRight: 1 }} />
-                  <Typography variant="body1">Docs</Typography>
-                </MenuItem>
-                <MenuItem>
+            {isMobile ? (
+              <Hidden mdUp>
+                <IconButton onClick={handleMenuClick} edge="start">
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  anchorEl={anchorEl}
+                  open={Boolean(anchorEl)}
+                  onClose={handleMenuClose}
+                >
+                  <MenuItem onClick={handleToolsClick}>
+                    <BuildIcon sx={{ marginRight: 1 }} />
+                    <Typography variant="body1">Tools</Typography>
+                  </MenuItem>
+                  <MenuItem onClick={handleDocsClick}>
+                    <DescriptionIcon sx={{ marginRight: 1 }} />
+                    <Typography variant="body1">Docs</Typography>
+                  </MenuItem>
+                  <MenuItem>
+                    <Switch checked={darkMode} onChange={handleSwitchToggle} />
+                  </MenuItem>
+                </Menu>
+              </Hidden>
+            ) : (
+              <Hidden smDown>
+                <Stack direction="row" alignItems="center">
+                  <IconButton onClick={handleToolsClick} edge="start">
+                    <BuildIcon sx={{ margin: 1 }} />
+                  </IconButton>
+                  <IconButton onClick={handleDocsClick} edge="start">
+                    <DescriptionIcon sx={{ margin: 1 }} />
+                  </IconButton>
                   <Switch checked={darkMode} onChange={handleSwitchToggle} />
-                </MenuItem>
-              </Menu>
-            </Stack>
+                </Stack>
+              </Hidden>
+            )}
           </div>
         </div>
       </Toolbar>
