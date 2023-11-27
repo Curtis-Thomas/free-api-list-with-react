87790591b374f2d743e2f19eb6bc9d4a0d0dc00f
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2023-11-27 11:23:11 +0200

    chore: .env

--- src/documentation/ContributorsContent.js
+++ src/documentation/ContributorsContent.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Octokit } from "@octokit/core";
-import { Box, Typography, Link, Table, TableHead, TableBody, TableRow, TableCell } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Link,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@mui/material";
 
 const ContributorsContent = () => {
   const [contributors, setContributors] = useState([]);
@@ -10,31 +19,41 @@ const ContributorsContent = () => {
   useEffect(() => {
     const fetchData = async () => {
       const octokit = new Octokit({
-        //recommended to use env variables
-        auth: `GitHub access token`, //add the access token here for auth, https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
+        auth: process.env.REACT_APP_GITHUB_TOKEN,
       });
 
       try {
-        const res = await octokit.request("GET /repos/Curtis-Thomas/free-api-list-with-react/contributors", {
-          owner: "Curtis-Thomas",
-          repo: "free-api-list-with-react",
-          headers: {
-            "X-GitHub-Api-Version": "2022-11-28",
-          },
-        });
+        const res = await octokit.request(
+          "GET /repos/Curtis-Thomas/free-api-list-with-react/contributors",
+          {
+            owner: "Curtis-Thomas",
+            repo: "free-api-list-with-react",
+            headers: {
+              "X-GitHub-Api-Version": "2022-11-28",
+            },
+          }
+        );
 
         const contributorsWithData = await Promise.all(
           res.data.map(async (contributor) => {
-            const userRes = await octokit.request(`GET /users/${contributor.login}`);
-            const eventsRes = await octokit.request(`GET /users/${contributor.login}/events`);
+            const userRes = await octokit.request(
+              `GET /users/${contributor.login}`
+            );
+            const eventsRes = await octokit.request(
+              `GET /users/${contributor.login}/events`
+            );
 
             // Find the latest push event
-            const pushEvent = eventsRes.data.find((event) => event.type === "PushEvent");
+            const pushEvent = eventsRes.data.find(
+              (event) => event.type === "PushEvent"
+            );
 
             return {
               ...contributor,
               name: userRes.data.name || contributor.login,
-              lastContributionDate: pushEvent ? new Date(pushEvent.created_at) : null,
+              lastContributionDate: pushEvent
+                ? new Date(pushEvent.created_at)
+                : null,
             };
           })
         );
@@ -80,14 +99,25 @@ const ContributorsContent = () => {
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell style={{ color: "black" }} onClick={() => handleSort("name")}>Name</TableCell>
-            <TableCell style={{ color: "black" }} onClick={() => handleSort("contributions")}>
+            <TableCell
+              style={{ color: "black" }}
+              onClick={() => handleSort("name")}
+            >
+              Name
+            </TableCell>
+            <TableCell
+              style={{ color: "black" }}
+              onClick={() => handleSort("contributions")}
+            >
               Contributions
               {sortCriteria === "contributions" && (
                 <span>{sortOrder === "asc" ? " ↑" : " ↓"}</span>
               )}
             </TableCell>
-            <TableCell style={{ color: "black" }} onClick={() => handleSort("lastContributionDate")}>
+            <TableCell
+              style={{ color: "black" }}
+              onClick={() => handleSort("lastContributionDate")}
+            >
               Last Contribution Date
               {sortCriteria === "lastContributionDate" && (
                 <span>{sortOrder === "asc" ? " ↑" : " ↓"}</span>
@@ -99,15 +129,23 @@ const ContributorsContent = () => {
         <TableBody>
           {sortedContributors.map((contributor) => (
             <TableRow key={contributor.id}>
-              <TableCell style={{ color: "black" }}>{contributor.name}</TableCell>
-              <TableCell style={{ color: "black" }}>{contributor.contributions}</TableCell>
+              <TableCell style={{ color: "black" }}>
+                {contributor.name}
+              </TableCell>
+              <TableCell style={{ color: "black" }}>
+                {contributor.contributions}
+              </TableCell>
               <TableCell style={{ color: "black" }}>
                 {contributor.lastContributionDate
                   ? contributor.lastContributionDate.toDateString()
                   : "No contributions"}
               </TableCell>
               <TableCell>
-                <Link href={contributor.html_url} target="_blank" rel="noopener noreferrer">
+                <Link
+                  href={contributor.html_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   GitHub
                 </Link>
               </TableCell>
