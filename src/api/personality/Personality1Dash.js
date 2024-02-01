8a97aefd74561c0ddc05afd23b2825964b488dde
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2024-02-01 16:23:12 +0200

    style: theme colors added to all personality pages

--- src/api/personality/Personality1Dash.js
+++ src/api/personality/Personality1Dash.js
@@ -1,10 +1,18 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, useTheme } from "@mui/material";
 import NavBar from "../../navigation/NavBar";
 import CardRouting from "../../components/CardRouting";
 
 const PersonalityDash = () => {
+  const theme = useTheme();
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box
+      sx={{
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        minHeight: "110vh",
+      }}
+    >
       <NavBar link="/PersonalityDash" />
       <Grid container spacing={2} sx={{ p: 3 }}>
         <Grid item xs={12} sm={6} md={4} lg={3}>
