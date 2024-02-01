036c5817ae4f7cefa23386ca6d9f324b0b8872f2
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2024-02-01 16:20:17 +0200

    style: theme colors added to all geocoding pages

--- src/api/geocoding/GeoCodingGeoJS.js
+++ src/api/geocoding/GeoCodingGeoJS.js
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import BoxApiInfo from "../../core/main/BoxApiInfo";
 import Endpoints from "../../components/Endpoints";
 import NavBar from "../../navigation/NavBar";
@@ -6,25 +6,45 @@ import ApiPageInfo from "../../core/main/ApiPageInfo";
 import BoxTestCrud from "../../tools/components/BoxTestCrud";
 
 const GeoCodingGeoJS = () => {
+  const theme = useTheme();
+
   return (
-    <Box>
-        <NavBar link="/GeoCodingDash" route2="/GeoJS" />
-        <ApiPageInfo
+    <Box
+      sx={{
+        backgroundColor: theme.palette.background.default,
+        color: theme.palette.text.primary,
+        minHeight: "120vh",
+      }}
+    >
+      <NavBar link="/GeoCodingDash" route2="/GeoJS" />
+      <ApiPageInfo
         title={"GeoJS"}
         linkUrl={"https://www.geojs.io/docs/v1/endpoints/"}
         description={
           "A REST/JSON/JSONP IP Geolocation lookup API without any rate limits"
         }
+      />
+      <BoxTestCrud />
+      <Box>
+        <BoxApiInfo baseEndpoint="https://get.geojs.io/v1" />
+        <Endpoints
+          header="GET"
+          endpoint="/ip.json"
+          description="This endpoint provides the requester's IP address. Please note that multiple IP lookups are not supported on this endpoint. If you prefer the response in plain text, you may remove the '.json' from the endpoint."
         />
-        <BoxTestCrud />
-        <Box>
-            <BoxApiInfo baseEndpoint="https://get.geojs.io/v1"/>
-            <Endpoints header="GET" endpoint="/ip.json" description="This endpoint provides the requester's IP address. Please note that multiple IP lookups are not supported on this endpoint. If you prefer the response in plain text, you may remove the '.json' from the endpoint." />
-            <Endpoints header="GET" endpoint="/ip/country.json" description="This endpoint provides information about the country associated with the requesting IP. There are multiple plain text endpoints available, allowing you to specify whether you prefer the full country name or just the country code. If you wish to receive only the country code, you can simply remove the '.json' from the endpoint." />
-            <Endpoints header="GET" endpoint="/ip/geo.json" description="This endpoint contains geographical information about an IP, such as its latitude and longitude, along with the owning (ASN) organisiation." />
-        </Box>
+        <Endpoints
+          header="GET"
+          endpoint="/ip/country.json"
+          description="This endpoint provides information about the country associated with the requesting IP. There are multiple plain text endpoints available, allowing you to specify whether you prefer the full country name or just the country code. If you wish to receive only the country code, you can simply remove the '.json' from the endpoint."
+        />
+        <Endpoints
+          header="GET"
+          endpoint="/ip/geo.json"
+          description="This endpoint contains geographical information about an IP, such as its latitude and longitude, along with the owning (ASN) organisiation."
+        />
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default GeoCodingGeoJS
+export default GeoCodingGeoJS;
--- src/api/geocoding/GeocodingAdministrativeDivisionsDb.js
+++ src/api/geocoding/GeocodingAdministrativeDivisionsDb.js
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import BoxApiInfo from "../../core/main/BoxApiInfo";
 import Endpoints from "../../components/Endpoints";
 import NavBar from "../../navigation/NavBar";
@@ -6,23 +6,37 @@ import ApiPageInfo from "../../core/main/ApiPageInfo";
 import BoxTestCrud from "../../tools/components/BoxTestCrud";
 
 const GeoCodingAdministrativeDivisionsDb = () => {
+  const theme = useTheme();
+
   return (
-    <Box>
-        <NavBar link="/GeoCodingDash" route2="/administrativeDivisionsDb" />
-        <ApiPageInfo
+    <Box
+      sx={{
+        backgroundColor: theme.palette.background.default,
+        color: theme.palette.text.primary,
+        minHeight: "120vh",
+      }}
+    >
+      <NavBar link="/GeoCodingDash" route2="/administrativeDivisionsDb" />
+      <ApiPageInfo
         title={"Administrative-Divisions-Db"}
-        linkUrl={"https://github.com/kamikazechaser/administrative-divisions-db"}
+        linkUrl={
+          "https://github.com/kamikazechaser/administrative-divisions-db"
+        }
         description={
           "This repo automagically serves an 'API' over Rawgit (Cloudflare network). No limits and uptime is guranteed."
         }
+      />
+      <BoxTestCrud />
+      <Box>
+        <BoxApiInfo baseEndpoint="https://rawcdn.githack.com/kamikazechaser/administrative-divisions-db/master/api" />
+        <Endpoints
+          header="GET"
+          endpoint="/US.json"
+          description="As an example, To get the administrative division of a particular country, simply make a GET request to the above endpoint and enter the country code in the endpoints as shown in above example "
         />
-        <BoxTestCrud />
-        <Box>
-            <BoxApiInfo baseEndpoint="https://rawcdn.githack.com/kamikazechaser/administrative-divisions-db/master/api"/>
-            <Endpoints header="GET" endpoint="/US.json" description="As an example, To get the administrative division of a particular country, simply make a GET request to the above endpoint and enter the country code in the endpoints as shown in above example " />
-        </Box>
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default GeoCodingAdministrativeDivisionsDb
+export default GeoCodingAdministrativeDivisionsDb;
