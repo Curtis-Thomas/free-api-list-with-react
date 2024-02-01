ddbbae2ae6387dbcfe80130627407878bc78a64f
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2024-02-01 16:14:59 +0200

    style: theme colors added to all dictionary pages

--- src/api/dictionary/DictionaryFreeDictionary.js
+++ src/api/dictionary/DictionaryFreeDictionary.js
@@ -1,30 +1,41 @@
-import React from 'react'
-import { Box } from "@mui/material";
+import React from "react";
+import { Box, useTheme } from "@mui/material";
 import BoxApiInfo from "../../core/main/BoxApiInfo";
 import Endpoints from "../../components/Endpoints";
 import NavBar from "../../navigation/NavBar";
 import ApiPageInfo from "../../core/main/ApiPageInfo";
 import BoxTestCrud from "../../tools/components/BoxTestCrud";
 
-
 const DictionaryFreeDictionary = () => {
+  const theme = useTheme();
+
   return (
-    <Box>
-        <NavBar link="/DictionaryDash" route2="/FreeDictionary" />
-        <ApiPageInfo
+    <Box
+      sx={{
+        backgroundColor: theme.palette.background.default,
+        color: theme.palette.text.primary,
+        minHeight: "120vh",
+      }}
+    >
+      <NavBar link="/DictionaryDash" route2="/FreeDictionary" />
+      <ApiPageInfo
         title={"Free Dictionary"}
         linkUrl={"https://dictionaryapi.dev/"}
         description={
           "Free Dictionary Api is a REST API to get definition of any English Word."
         }
+      />
+      <BoxTestCrud />
+      <Box>
+        <BoxApiInfo baseEndpoint="https://api.dictionaryapi.dev/api/v2/entries/en" />
+        <Endpoints
+          header="GET"
+          endpoint="/hello"
+          description="As an example, to get definition of English word hello, you can send request to the above endpoint, you can choose any word of your choice by modifying the 'hello' in the above endpoint "
         />
-        <BoxTestCrud />
-        <Box>
-            <BoxApiInfo baseEndpoint="https://api.dictionaryapi.dev/api/v2/entries/en"/>
-            <Endpoints header="GET" endpoint="/hello" description="As an example, to get definition of English word hello, you can send request to the above endpoint, you can choose any word of your choice by modifying the 'hello' in the above endpoint " />
-        </Box>
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default DictionaryFreeDictionary
+export default DictionaryFreeDictionary;
