ea6afdad9269c92418a6a972297872202e4b81d0
AJ-KUSHWAH <kushwahaj2707@gmail.com>
2024-02-07 15:24:13 +0530

    style: theme coloring added to MachineLearning1Dash

--- src/api/machineLearning/MachineLearning1Dash.js
+++ src/api/machineLearning/MachineLearning1Dash.js
@@ -1,10 +1,13 @@
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import BtnRouting from "../../components/BtnRouting";
 import NavBar from "../../navigation/NavBar";
 
 const MachineLearningDash = () => {
+
+  const theme = useTheme();
+
   return (
-    <Box>
+    <Box  sx={{ flexGrow: 1, backgroundColor: theme.palette.background.default, minHeight: "110vh",}}>
       <NavBar link="/MachineLearningDash" />
       <Box>
         <BtnRouting btnName="" navigateTo="" />
