5007ecbcbd149c935effd54c5f2245493182a4ee
Atharva Nimbalkar <g.brain2753@gmail.com>
2024-04-01 19:33:17 +0530

    add: BooksPoetryDb

--- src/api/development/Development1Dash.js
+++ src/api/development/Development1Dash.js
@@ -1,6 +1,6 @@
-import { Box, useTheme } from "@mui/material";
-import BtnRouting from "../../components/BtnRouting";
+import { Box,Grid, useTheme } from "@mui/material";
 import NavBar from "../../navigation/NavBar";
+import CardRouting from "../../components/CardRouting";
 
 const DevelopmentDash = () => {
   const theme = useTheme();
@@ -14,8 +14,21 @@ const DevelopmentDash = () => {
       }}
     >
       <NavBar link="/DevelopmentDash" />
+      
+      {/* Cards in a responsive grid layout */}
+      <Grid container spacing={2} sx={{ p: 3 }}>
+        {/* Card for Development */}
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <CardRouting
+            cardName="Cloudflare Trace"
+            navigateTo="DevelopmentCloudFlareTrace"
+            tags="Development"
+            description="Get IP Address, TimeStamp, User Agent,"
+            difficulty="Easy"
+          />
+        </Grid>
+        </Grid>
       <Box>
-        <BtnRouting btnName="" navigateTo="" />
       </Box>
     </Box>
   );
